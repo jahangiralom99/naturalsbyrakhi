c15b23bb155a4b4b323979e9c8af4b3997a12bd6
jahangir <jahangiralomtuf99@gmail.com>
2024-08-08 15:23:12 +0600

    added utilities

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
+import { createContext } from "react";
+import { Outlet } from "react-router-dom";
 
-import { Outlet } from 'react-router-dom'
+export const ItemContext = createContext([]);
 
 function App() {
-
   return (
-    <div className='bg-[#f5f5f5]'>
-      <Outlet/>      
+    <div className="bg-[#f5f5f5]">
+      <ItemContext.Provider value={}>
+      <Outlet />
+     </ItemContext.Provider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
